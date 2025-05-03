765a6795dd8dbd606795d296aee16c674f50a6d5
Dennis Parolin <dennis.par20@gmail.com>
2025-05-03 16:01:53 +0200

    Aggiunto il test per i primi 3 numeri, close #2

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -4,7 +4,14 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class IntegerToRomanTest {
-    
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -4,5 +4,14 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class RomanPrinterTest {
+    @Test
+    public void testPrint() {
+        assertEquals("  __ \n |   |\n   | |  \n   | |  \n  | | \n |__|\n", RomanPrinter.print(1));
+        assertEquals("      \n |   | |   |\n   | |     | |  \n   | |     | |  \n  | |   | | \n || ||\n", RomanPrinter.print(2));
+        assertEquals("        __ \n |   | |   | |   |\n   | |     | |     | |  \n   | |     | |     | |  \n  | |   | |   | | \n |__| || ||\n", RomanPrinter.print(3));
+    }
 }
