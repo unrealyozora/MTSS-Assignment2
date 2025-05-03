bdebd71c8a79ea7bb5a72e6f8a8272f71491da13
unrealyozora <tommaso.ceron@gmail.com>
2025-05-03 15:48:33 +0200

    Caricati i file iniziali, close #1

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////////////////////////// 
+// [Tommaso] [Ceron] [2101045] 
+// [Dennis] [Parolin] [2113203] 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert (int number){
+        return null;
+    }
+}
---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////// 
+// [Tommaso] [Ceron] [2101045] 
+// [Dennis] [Parolin] [2113203] 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num){
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+    private static String printAsciiArt(String romanNumber){
+        return null;
+    }
+}
