75355773437de7697f56eb2fe22b7282f4489630
Dennis Parolin <dennis.par20@gmail.com>
2025-05-04 16:19:11 +0200

    Aggiunto il codice per gestire i casi limite, close #19

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,6 +6,11 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert (int number){
+
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Invalid number: " + number + ". Number must be between 1 and 1000.");
+        }
+
         String[] romanLetters = {"M", "CM","D","CD","C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int[] values = {1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
